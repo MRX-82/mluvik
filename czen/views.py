3622296608cc6b3c630d9a2bcd38ed387b348073
Luky1987 <82019.is@gmail.com>
2024-01-07 21:44:17 +0300

    Написана функция-представление для домашней страницы и url-связь с ней.

--- czen/views.py
+++ czen/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import User, Mluvi
 
-# Create your views here.
+
+def imdex(request):
+    """
+    Home page
+    """
+    return render(request, "index.html")
