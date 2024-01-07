8cbcdc25d89f982cf1c3b2bee421c29141633d89
Luky1987 <82019.is@gmail.com>
2024-01-07 21:13:40 +0300

    Создано две модели, для хранения пользователей, и для хранения слов с переводом.

--- czen/models.py
+++ czen/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """
+    This is model for users
+    """
+    login = models.CharField(min_length=4, max_length=20)
+    password = models.CharField(min_length=4, max_length=20)
+    my_language = models.CharField(max_length=20)
+    new_language = models.CharField(max_length=20)
+
+
+class Mluvi(models.Model):
+    """
+    This is model for language
+    """
+    user = models.ForeingKey(User, on_delete=models.CASCADE)
+    my_word = models.CharField(max_length=25)
+    new_word = models.CharField(max_length=25)
+
