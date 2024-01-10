0856594a51128da365df9f21ac649095de79d315
Luky1987 <82019.is@gmail.com>
2024-01-10 21:06:14 +0300

    Исправлены ошибки в файле logika.py функция def add_learning(), исправлены строки отвечающие за сортировку.

--- czen/logika.py
+++ czen/logika.py
@@ -19,23 +19,23 @@ def added_word(my_word, new_word, user_id):
     """
     user_cl = User.objects.get(id=user_id)
     words = Mluvi()
-    #words.my_word()
     words.my_word = my_word
     words.new_word = new_word
     words.user = user_cl
-    #words = Mluvi.objects.create(my_word = my_word, new_word = new_word)
     words.save()
 
 
 def word_learning(user_id):
     """
-    This function load word for learning
+    This function load word for learning, one session = 20 small status_word word
     """
     word_lern = []
-    words_all = Mluvi.objects.all()
+    words_all_first = Mluvi.objects.all()
+    words_all = words_all_first.filter(user_id=user_id)
     for word in words_all:
         word_lern.append((word.new_word, word.my_word, word.status_word))
     word_lern_full_sort = sorted(word_lern, key=lambda x: x[2])
+    word_lern_full_sort = word_lern_full_sort[:21]
     return word_lern_full_sort
 
 
